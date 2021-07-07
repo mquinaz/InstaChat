b3acb89a93022ab68aa951383957fc6e0ef16230
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-07-07 23:36:26 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-#pip install pyautogui
-#pip install opencv-python
-#pip install Pillow
 from time import sleep
 import pyautogui as pt
 import pyperclip as pc
 
-if __name__ == '__main__':
-    print('PyCharm')
+sleep(2)
 
+def move_to_text_input(message):
+    position = pt.locateOnScreen('images/photo.png', confidence=.7)
+    pt.moveTo(position[0:2], duration=.5)
+    pt.moveRel(-100, 20, duration=.5)
+    pt.doubleClick(interval=.3)
+
+    pt.typewrite(message, interval=.01)
+    pt.typewrite('\n')
+
+if __name__ == '__main__':
+    move_to_text_input('hello')
