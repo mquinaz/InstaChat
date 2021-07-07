5c366907b9d19916e03174793daf9b0268c65dc0
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-07-07 23:08:52 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#pip install pyautogui
+#pip install opencv-python
+#pip install Pillow
+from time import sleep
+import pyautogui as pt
+import pyperclip as pc
+
+if __name__ == '__main__':
+    print('PyCharm')
+
