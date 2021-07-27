3cf123f94d38065bd69be52d741f1b40167f124f
mquinaz <36314718+mquinaz@users.noreply.github.com>
2021-07-27 19:32:35 +0100

    Changed response complexity

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from time import sleep
 import pyautogui as pt
 import pyperclip as pc
 
-sleep(2)
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
 
 #Moves cursor to text input box to respond
 def move_to_text_input(message):
@@ -35,60 +41,72 @@ def get_messages():
     user_text = pc.paste()
     return user_text
 
-#Process message
-def process_message(message):
-    msg = str(message).lower()
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
 
-    if msg == 'oi':
-        return 'Tudo bem linda'
-    elif msg == 'bebezinho':
-        return 'bebezinha'
-    elif msg == 'hellooo':
-        return 'Agora me falas em ingles sua vadia e o espanhol?'
-    elif msg == 'hahahahha':
-        return 'nao te rias de mim pfv'
-    elif msg == 'eu ein':
-        return 'eu o carago'
-    elif msg == 'pode ir':
-        return 'Para onde princesa?'
-    elif msg == 'dr susana':
-        return 'A tua chefe e um exemplo de profissional..ela faz cirurgia com talento'
-    elif msg == 'to com fome':
-        return 'Posso cozinhar para ti imensos pratos deliciosos, queres experimentar?'
-    elif msg == 'nope':
-        return 'yap'
-    elif msg == 'então':
-        return 'ent eu gosto disso mas tu nao o que queres?'
-    else:
-        return 'conta-me uma historia please'
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intents': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb') )
+classes = pickle.load(open('classes.pkl', 'rb') )
+model = load_model('chatbot_model.model')
 
 last_message, last_response = '', ''
 
-def chat_Box():
-    global last_message, last_response
-
-    current_message = get_messages()
-    print(current_message)
-
-    if current_message != last_message:
-        last_message = current_message
-        print(f'Last copied message: {current_message}')
-
-        # Bot response
-        if current_message != last_response:
-            response = process_message(current_message)
-            last_response = response
-            print(f'Bot: {response}')
-            move_to_text_input(response)
-
-    else:
-        print('No new messages ')
-
 if __name__ == '__main__':
+    sleep(2)
+    global last_message, last_response
     while True:
         try:
-            chat_Box()
-            sleep(10)
+            current_message = get_messages()
+            print(current_message)
+
+            if current_message != last_message:
+                last_message = current_message
+                print(f'Last copied message: {current_message}')
+
+                # Bot response
+                if current_message != last_response:
+                    response_Class = predict_class(current_message)
+                    response = get_response(response_Class, intents)
+                    last_response = response
+                    print(f'Bot: {response}')
+                    move_to_text_input(response)
+            else:
+                print('No new messages ')
+            sleep(8)
         except Exception as e:
             print(f'Exception {e}')
-            sleep(10)
+            sleep(8)
