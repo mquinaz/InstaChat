d3d7140b138219bd44fb435aa10f9515f512154c
mquinaz <36314718+mquinaz@users.noreply.github.com>
2022-01-20 17:56:08 +0000

    Not filling lists for json serialization

--- intentGeneration.py
+++ intentGeneration.py
@@ -1,29 +1,49 @@
 import csv
+import json
 
-my_name = input()
-target_Name = input()
+my_name = "Miguel"#input()
+target_Name = "Laura  Munhoz ⚓"#input()
 #Laura  Munhoz ⚓ Miguel
 
 flag = 0
+count = 0
+dictAuxFile = {}
+listDict = []
+
 with open('dataset.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         text = row['messages__content']
         if text == "":
             continue
+
         listTargetwords = []
         listResponses = []
         if(row['messages__sender_name'] == my_name):
             print(text)
+            if flag == 1:
+                listResponses.append(text)
+                flag = 2
         else:
-            flag = 1
-            listTargetwords.append(text)
+            if(flag > 1):
+                flag = 0
+                count += 1
+                dictAuxFile["tag"] = str(count)
+                dictAuxFile["patterns"] = listTargetwords
+                dictAuxFile["responses"] = listResponses
+                listDict.append(dictAuxFile)
+            else:
+                flag = 1
+                listTargetwords.append(text)
+                print(text)
 
-            #tokens = nltk.word_tokenize("And now for something completely different")
-            #tagged_sent = nltk.pos_tag(tokens)
-            #simplified = [(word, simplify_wsj_tag(tag)) for word, tag in tagged_sent]
-            #print(simplified)
-
-            print(text + "&")
 
 
+fileIntent = {"intents":listDict}
+
+# Serializing json
+json_object = json.dumps(fileIntent, indent=1)
+
+# Writing to sample.json
+with open("intentExample.json", "w") as outfile:
+    outfile.write(json_object)
